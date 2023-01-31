104e48673f9a2eebf4e92d74028645bcc0fd16cc
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2023-01-31 17:47:29 +0100

    init configuration

--- README.md
+++ README.md
@@ -1,2 +1,142 @@
-# watson-nlp-custom-init-container
-This project does show how to create an init container with a custom model for Watson NLP.
+# Create an init container with a custom model
+
+This project does show how to create an init container with a custom model for `Watson NLP for Embed` and upload the container to IBM Cloud container registry.
+
+The project reuses information from the tutorial [_`Serve a custom model on a Kubernetes or Red Hat OpenShift cluster`_](https://developer.ibm.com/tutorials/serve-custom-models-on-kubernetes-or-openshift/).
+
+First let us resume how you can add models to `Watson NLP for Embed` runtime container.
+
+1. You can build a `Watson NLP for Embed` runtime container including the models.
+2. You can run a `Watson NLP for Embed` runtime and reference models from a linked location.
+3. Serve the model for [KServe](https://suedbroecker.net/2023/01/17/run-watson-nlp-for-embed-in-a-kserve-modelmesh-serving-environment-on-an-ibm-cloud-kubernetes-cluster-in-a-vpc-environment/)
+4. And, now build a custom model container image!
+
+### Step 1: Prepare python environment on your local machine
+
+We need a python library manages the process of building a collection of docker images that wrap individual `watson_embedded models` for delivery with an `embeddable watson runtime`.
+
+This is implemented in the GitHub project called [`ibm-watson-embed-model-builder`](https://github.com/IBM/ibm-watson-embed-model-builder).
+
+```sh
+python3 -m venv client-env
+source client-env/bin/activate
+pip install watson-embed-model-packager
+```
+
+### Step 2: Download a created model and copy it to the folder `code/models`
+
+If you don't have a created model you can create one by following this blog post [`Watson NLP for Embed customize a classification model and use it on your local machine`](https://suedbroecker.net/2023/01/26/watson-nlp-for-embed-customize-a-classification-model-and-use-it-on-your-local-machine/).
+
+### Step 3: Unzip your custom model
+
+```sh
+cd code/models
+export MODELFILE_NAME=ensemble_model
+mkdir custom_model
+unzip $(pwd)/$MODELFILE_NAME -d $(pwd)/custom_model/
+```
+
+### Step 4: Prepare the custom model container image 
+
+```sh
+cd ..
+export CUSTOM_MODEL_LOCATION=$(pwd)/models
+export CUSTOM_TAG=1.0.0
+python -m watson_embed_model_packager setup \
+    --library-version watson_nlp:3.2.0 \
+    --image-tag 1.0.0 \
+    --local-model-dir $CUSTOM_MODEL_LOCATION \
+    --output-csv model-manifest.csv
+ls
+```
+
+* Example output:
+
+```sh
+..
+2023-01-31T12:52:42.622576 [SETUP:INFO] Running SETUP
+2023-01-31T12:52:42.622966 [SETUP:INFO] Library Versions: {'watson_nlp': VersionInfo(major=3, minor=2, patch=0, prerelease=None, build=None)}
+2023-01-31T12:52:42.623034 [SETUP:INFO] Local Model Dir: /YOUR_PATH/code/models
+2023-01-31T12:52:42.623099 [SETUP:INFO] Module GUIDs: []
+2023-01-31T12:52:42.623150 [SETUP:INFO] Image tag version: 1.0.0
+model-manifest.csv      models
+```
+
+### Step 5: Create the custom model container image 
+
+Now we build the `custom model container image` by using the `model-manifest.csv`.
+
+```sh
+python3 -m watson_embed_model_packager build --config model-manifest.csv
+```
+
+* Example output:
+
+```sh
+2023-01-31T12:57:33.601886 [BUILD:INFO] Building model [custom_model]
+2023-01-31T12:57:34.193581 [BUILD:INFO] Building with --platform
+[+] Building 105.4s (21/21) FINISHED                                        
+...
+ => => writing image sha256:4034108815be4eee1a1248e4032e646c136d0e74b  0.0s
+ => => naming to docker.io/library/watson-nlp_custom_model:1.0.0       0.0s
+```
+
+### Step 6: Verify the created model container image
+
+```sh
+docker images | grep watson-nlp_custom_model 
+```
+
+* Example output:
+
+```sh
+watson-nlp_custom_model                                               1.0.0         4034108815be   23 minutes ago   1.3GB
+```
+
+## Deploy to Kubernetes
+
+We are using for this section:
+
+* IBM Cloud Kubernetes cluster
+* IBM Cloud Container Registry
+
+### Step 1: Navigate to the Helm setup
+
+```sh
+cd helm_setup
+```
+
+### Step 2: Set environment variables in the `.env` file
+
+```sh
+cat .env_template > .env
+```
+
+Edit the `.env` file.
+
+```sh
+# used as 'environment' variables
+export IC_API_KEY=YOUR_IBM_CLOUD_ACCESS_KEY
+export IC_EMAIL="YOUR_EMAIL"
+export IBM_ENTITLEMENT_KEY="YOUR_KEY"
+export IBM_ENTITLEMENT_EMAIL="YOUR_EMAIL"
+export CLUSTER_ID="YOUR_CLUSTER"
+export REGION="us-east"
+export GROUP="tsuedbro"
+```
+
+### Step 3: Run the helm automation to deploy the model
+
+```sh
+sh deploy-watson-nlp-custom-to-kubernetes.sh
+```
+
+1. Log in to IBM Cloud.
+2. Configure the IBM Cloud registry and and a namespace if needed.
+3. Change the tag of the custoom container image and upload custom image to IBM Cloud registry container registry
+4. Create the Docker config file needed to create a pull secret for the custom container and the runtime container.
+5. Connect the Kubernetes Cluster
+6. Install `Helm Chart`
+7. Verify invocation from the running container
+8. Verify nvocation from the local machine
+
---
+++ code/helm_setup/deploy-watson-nlp-custom-to-kubernetes.sh
@@ -0,0 +1,327 @@
+#!/bin/bash
+
+# **************** Global variables
+source ./.env
+
+export HELM_RELEASE_NAME=watson-nlp-custom
+export DEFAULT_NAMESPACE="default"
+export CR_NAMESPACE="custom-watson-nlp-tsued"
+export CE_PROJECT_NAME="custom-watson-nlp-tsued"
+export WATSON_NLP_CONTAINER=watson-nlp-custom
+
+######### custom model Watson NLP image ##############
+CUSTOM_WATSON_NLP_IMAGE_NAME=watson-nlp_custom_model
+CUSTOM_TAG=1.0.0
+
+######### IBM Cloud Container registry for custom image ##############
+CR_CUSTOM_REGISTRY_URL=us.icr.io
+
+# **********************************************************************************
+# Functions definition
+# **********************************************************************************
+
+function loginIBMCloud () {
+    
+    echo ""
+    echo "*********************"
+    echo "loginIBMCloud"
+    echo "*********************"
+    echo ""
+
+    ibmcloud login --apikey $IC_API_KEY
+    ibmcloud target -r $REGION
+    ibmcloud target -g $GROUP
+}
+
+function configureIBMCloudRegistry () {
+    echo ""
+    echo "# ******"
+    echo "# Configure IBM Cloud Registry"
+    echo "# ******"
+    echo ""
+
+    ibmcloud cr region-set $CR_CUSTOM_REGISTRY_URL
+    ibmcloud cr namespace-add $CR_NAMESPACE
+    docker login -u iamapikey -p $IC_API_KEY $CR_CUSTOM_REGISTRY_URL
+    ibmcloud cr login
+}
+
+function uploadCustomImageToIBMCloudRegistry () {
+    # Tag the image
+    echo "Container image: ${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG"
+    docker tag $CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG ${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG
+
+    # Push the image
+    docker push ${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG
+}
+
+function connectToCluster () {
+
+    echo ""
+    echo "*********************"
+    echo "connectToCluster"
+    echo "*********************"
+    echo ""
+
+    ibmcloud ks cluster config -c $CLUSTER_ID
+    kubectl config current-context
+}
+
+function createDockerCustomConfigFile () {
+
+    echo ""
+    echo "*********************"
+    echo "createDockerCustomConfigFile"
+    echo "*********************"
+    echo ""
+
+    echo "- custom_config.json"
+    sed "s+IBM_ENTITLEMENT_KEY+$IBM_ENTITLEMENT_KEY+g;s+IBM_ENTITLEMENT_EMAIL+$IBM_ENTITLEMENT_EMAIL+g;s+IC_EMAIL+$IC_EMAIL+g;s+IC_API_KEY+$IC_API_KEY+g;s+CR_CUSTOM_REGISTRY_URL+$CR_CUSTOM_REGISTRY_URL+g" "$(pwd)/custom_config.json_template" > "$(pwd)/custom_config.json"
+    IBM_ENTITLEMENT_SECRET=$(base64 -i "$(pwd)/custom_config.json")
+    echo "IBM_ENTITLEMENT_SECRET: $IBM_ENTITLEMENT_SECRET"
+    
+    echo "- charts/values.yaml"
+    CUSTOM_MODEL=${CR_CUSTOM_REGISTRY_URL}/${CR_NAMESPACE}/$CUSTOM_WATSON_NLP_IMAGE_NAME:$CUSTOM_TAG
+    echo "Set values:"
+    echo "- $CUSTOM_MODEL"
+    echo "- $IBM_ENTITLEMENT_SECRET"
+
+    sed "s+IBM_ENTITLEMENT_SECRET+$IBM_ENTITLEMENT_SECRET+g;s+CUSTOM_MODEL+$CUSTOM_MODEL+g;" $(pwd)/charts/$HELM_RELEASE_NAME/values.yaml_template > $(pwd)/charts/$HELM_RELEASE_NAME/values.yaml
+    cat $(pwd)/charts/$HELM_RELEASE_NAME/values.yaml
+}
+
+function installHelmChart () {
+
+    echo ""
+    echo "*********************"
+    echo "installHelmChart"
+    echo "*********************"
+    echo ""
+
+    TEMP_PATH_ROOT=$(pwd)
+    cd $TEMP_PATH_ROOT/charts
+    
+    helm dependency update ./$HELM_RELEASE_NAME/
+    helm install --dry-run --debug helm-test
+
+    helm lint ./$HELM_RELEASE_NAME
+    helm install $HELM_RELEASE_NAME ./$HELM_RELEASE_NAME
+
+    verifyDeploment
+    verifyPod
+        
+    cd $TEMP_PATH_ROOT
+}
+
+function verifyWatsonNLPContainer () {
+    
+    echo ""
+    echo "*********************"
+    echo "verifyWatsonNLPContainer"
+    echo "*********************"
+    echo ""
+
+    export FIND=$WATSON_NLP_CONTAINER
+    POD=$(kubectl get pods -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $1;}')
+    echo "Pod: $POD"
+    # Needs to be verifed
+    # COMMAND='''curl -X POST "http://localhost:8080/v1/watson.runtime.nlp.v1/NlpService/SyntaxPredict" -H "accept: application/json" -H "grpc-metadata-mm-model-id: syntax_izumo_lang_en_stock" -H "content-type: application/json" -d " { \"rawDocument\": { \"text\": \"It is so easy to embed Watson NLP in application. Very cool\" }}"'''
+    RESULT=$(kubectl exec --stdin --tty $POD --container $FIND -- curl -X POST "http://localhost:8080/v1/watson.runtime.nlp.v0/NlpService/ClassificationPredict" -H "accept: application/json" -H "grpc-metadata-mm-model-id: custom_model" -H "content-type: application/json" -d '{ "rawDocument": { "text": "The credit card does not work, and I look at the savings, but I need more money to spend." }}')
+    echo ""
+    echo "Result of the Watson NLP API request:"
+    echo "http://localhost:8080/v1/watson.runtime.nlp.v0/NlpService/ClassificationPredict"
+    echo ""
+    echo "$RESULT"
+    echo ""
+    echo "Verify the running pod on your cluster."
+    kubectl get pods -n $DEFAULT_NAMESPACE
+    echo "Verify in the deployment in the Kubernetes dashboard."
+    echo ""
+    open "https://cloud.ibm.com/kubernetes/clusters/$CLUSTER_ID/overview"
+    echo ""
+
+    read ANY_VALUE
+}
+
+function verifyWatsonNLPLoadbalancer () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyWatsonNLP_loadbalancer"
+    echo "this could take up to 10 min"
+    echo "*********************"
+    echo ""
+
+    verifyLoadbalancer
+
+    SERVICE=watson-nlp-custom-vpc-nlb
+    EXTERNAL_IP=$(kubectl get svc $SERVICE | grep  $SERVICE | awk '{print $4;}')
+    echo "EXTERNAL_IP: $EXTERNAL_IP"
+    echo "Verify invocation of Watson NLP API from the local machine:"
+    curl -X POST "http://$EXTERNAL_IP:8080/v1/watson.runtime.nlp.v0/NlpService/ClassificationPredict" -H "accept: application/json" -H "grpc-metadata-mm-model-id: watson-nlp_custom_model:1.0.0" -H "content-type: application/json" -d '{ "rawDocument": { "text": "The credit card does not work, and I look at the savings, but I need more money to spend." }}'
+}
+
+function uninstallHelmChart () {
+
+    echo ""
+    echo "*********************"
+    echo "uninstallHelmChart"
+    echo "*********************"
+    echo ""
+
+    echo "Press any key to move on with UNINSTALL:"
+    read ANY_VALUE
+
+    helm uninstall $HELM_RELEASE_NAME
+}
+
+# ************ functions used internal **************
+
+
+function verifyLoadbalancer () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyLoadbalancer"
+    echo "*********************"
+    echo ""
+
+    export max_retrys=10
+    j=0
+    array=("watson-nlp-custom-vpc-nlb")
+    export STATUS_SUCCESS=""
+    for i in "${array[@]}"
+        do
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check for $i: ($j) from max retrys ($max_retrys)"
+            j=0
+            export FIND=$i
+            while :
+            do      
+            ((j++))
+            STATUS_CHECK=$(kubectl get svc $FIND -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $4;}')
+            echo "Status: $STATUS_CHECK"
+            if ([ "$STATUS_CHECK" != "$STATUS_SUCCESS" ] && [ "$STATUS_CHECK" != "<pending>" ]); then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is created ($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                    break
+                elif [[ $j -eq $max_retrys ]]; then
+                    echo "$(date +'%F %H:%M:%S') Maybe a problem does exists!"
+                    echo "------------------------------------------------------------------------"
+                    exit 1              
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 60
+            done
+        done
+}
+
+function verifyDeploment () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyDeploment"
+    echo "*********************"
+    echo ""
+
+    export max_retrys=4
+    j=0
+    array=($WATSON_NLP_CONTAINER)
+    export STATUS_SUCCESS=$WATSON_NLP_CONTAINER
+    for i in "${array[@]}"
+        do
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check for ($i)"
+            j=0
+            export FIND=$i
+            while :
+            do      
+            ((j++))
+            echo "($j) from max retrys ($max_retrys)"
+            STATUS_CHECK=$(kubectl get deployment $FIND -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $1;}')
+            echo "Status: $STATUS_CHECK"
+            if [ "$STATUS_CHECK" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is created"
+                    echo "------------------------------------------------------------------------"
+                    break
+                elif [[ $j -eq $max_retrys ]]; then
+                    echo "$(date +'%F %H:%M:%S') Maybe a problem does exists!"
+                    echo "------------------------------------------------------------------------"
+                    exit 1              
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 10
+            done
+        done
+}
+
+function verifyPod () {
+
+    echo ""
+    echo "*********************"
+    echo "verifyPod could take 10 min"
+    echo "*********************"
+    echo ""
+
+    export max_retrys=10
+    j=0
+    array=($WATSON_NLP_CONTAINER)
+    export STATUS_SUCCESS="1/1"
+    for i in "${array[@]}"
+        do
+            echo ""
+            echo "------------------------------------------------------------------------"
+            echo "Check for ($i)"
+            j=0
+            export FIND=$i
+            while :
+            do     
+            ((j++))
+            echo "($j) from max retrys ($max_retrys)"
+            STATUS_CHECK=$(kubectl get pods -n $DEFAULT_NAMESPACE | grep $FIND | awk '{print $2;}')
+            echo "Status: $STATUS_CHECK"
+            if [ "$STATUS_CHECK" = "$STATUS_SUCCESS" ]; then
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND is created"
+                    echo "------------------------------------------------------------------------"
+                    break
+                elif [[ $j -eq $max_retrys ]]; then
+                    echo "$(date +'%F %H:%M:%S') Maybe a problem does exists!"
+                    echo "------------------------------------------------------------------------"
+                    exit 1              
+                else
+                    echo "$(date +'%F %H:%M:%S') Status: $FIND($STATUS_CHECK)"
+                    echo "------------------------------------------------------------------------"
+                fi
+                sleep 60
+            done
+        done
+}
+
+
+#**********************************************************************************
+# Execution
+# *********************************************************************************
+
+loginIBMCloud
+
+# configureIBMCloudRegistry
+
+# uploadCustomImageToIBMCloudRegistry
+
+createDockerCustomConfigFile
+
+connectToCluster
+
+installHelmChart
+
+verifyWatsonNLPContainer
+
+verifyWatsonNLPLoadbalancer
+
+#uninstallHelmChart
